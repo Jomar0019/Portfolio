9930aeec5d0c154769e64f452c11f17da7874ea4
unknown <jomarmagpoot24@gmail.com>
2022-09-04 19:32:00 +0800

    Add link to resume button

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -5,8 +5,13 @@ const Me = () => {
     <div className='container mx-auto my-20 h-[75vh]  p-12 flex justify-center items-center' id='me'>
         <div className='text-center'>
             <h1 className='text-3xl lg:text-8xl font-bold'>I am Jomar Magpoot.</h1>
-            <h2 className='text-2xl lg:text-6xl font-semibold mt-5 lg:mt-10 text-gray-500'>Web Developer</h2>
-            <button className='mt-5 lg:mt-10 border border-black py-1 lg:py-3 px-5 lg:px-12 text-lg lg:text-xl hover:text-white hover:bg-black transition duration-300'>Resume</button>
+            <h2 className='text-2xl lg:text-6xl font-semibold mt-5 lg:my-10 text-gray-500'>Web Developer</h2>
+            <a 
+            href='https://docs.google.com/document/d/1gS1JFEoaSIxLm2xHs1mqc8BhvGcG17cg/edit?usp=sharing&ouid=109052017502887735938&rtpof=true&sd=true' 
+            className='mt-5 lg:mt-10 border border-black py-1 lg:py-3 px-5 lg:px-12 text-lg lg:text-xl hover:text-white hover:bg-black transition duration-300'
+            target='_blank'
+            rel='noreferrer'
+            >Resume</a>
         </div>
     </div>
   )
